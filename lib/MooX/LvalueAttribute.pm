4250f7c3fb5964501359fb73d947e3728c023d1f
Damien Krotkine <dams@zarb.org>
2013-02-11 21:44:20 +0100

    documentation

--- lib/MooX/LvalueAttribute.pm
+++ lib/MooX/LvalueAttribute.pm
@@ -26,4 +26,55 @@ sub import {
 
 }
 
+=head1 SYNOPSIS
+
+  package App;
+  use Moo;
+  use MooX::LvalueAttribute;
+  
+  has name => (
+    is => 'rw',
+    lvalue => 1,
+  );
+
+  # Elsewhere
+
+  my $app = App->new(name => 'foo');
+  $app->name = 'Bar';
+  print $app->name;  # Bar
+
+=cut
+
+=head1 DESCRIPTION
+
+B<WARNING>: this module is in its early stage, its API may change.
+
+This modules provides Lvalue accessors to your Moo attributes. It won't break
+Moo's encapsulation, and will properly call any accessor method modifiers,
+triggers, builders and default values creation.
+
+It means that instead of writing:
+
+  $object->name("Foo");
+
+you can use:
+
+  $object->name = "Foo"; 
+
+=head1 ATTRIBUTE SPECIFICATION
+
+To enable Lvalue access to your attribute, simply use C<MooX::LvalueAttribute>
+in the class, and add:
+
+  lvalue => 1,
+
+in the attribute specification (see synopsis).
+
+=head1 NOTE ON IMPLEMENTATION
+
+The implementation doesn't use AUTOLOAD, nor TIESCALAR. Instead, it uses a
+custom accessor and C<Variable::Magic>, which is faster than the tie mechanism.
+
+=cut
+
 1;
